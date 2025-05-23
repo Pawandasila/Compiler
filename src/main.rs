4a295d67a05c5fa36611209445a69893fd79e715
Pawan Dasila <pawandasila06@gmail.com>
2025-05-23 19:01:12 +0530

    changed port

--- src/main.rs
+++ src/main.rs
@@ -137,7 +137,7 @@ async fn main() -> std::io::Result<()> {
             .service(compile)
             .service(fs::Files::new("/", "./").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
